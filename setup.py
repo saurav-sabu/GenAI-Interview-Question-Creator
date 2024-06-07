3d194b9689601539944547ead5092bfbe521d8da
Saurav Sabu <saurav.sabu9@gmail.com>
2024-06-08 01:32:51 +0530

    Updated the code in setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="interview-question-creator",
+    version="0.0.0",
+    author="Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    description="A tool to create interview questions",
+    packages=find_packages(),
+    install_requires=[]
+)
