82f43d8d4e74318991914d07fdf9a4d33c03cd22
Saurav Sabu <saurav.sabu9@gmail.com>
2024-06-08 01:35:24 +0530

    Added comments for setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+# Import modules
 from setuptools import setup, find_packages
 
+# Configure the setup for the package
 setup(
     name="interview-question-creator",
     version="0.0.0",
     author="Saurav Sabu",
     author_email="saurav.sabu9@gmail.com",
     description="A tool to create interview questions",
-    packages=find_packages(),
-    install_requires=[]
+    packages=find_packages(), # Automatically find and include all packages in the project
+    install_requires=[] # List of dependencies required by the package
 )
